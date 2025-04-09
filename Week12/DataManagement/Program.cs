0c92fefbaec8e10940c9108e7fba954f41f75f1f
Uxue SAEZ FERNANDEZ <S00278527@atu.ie>
2025-04-09 11:27:36 +0100

    bh

---
+++ Week12/DataManagement/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week12;
+
+namespace DataManagement
+{
+    internal class Program
+    { 
+        static void Main(string[] args)
+        {
+            MovieData db = new MovieData();
+
+            using(db)
+            {
+                Movie m1 = new Movie() 
+                {
+                    Title= "The Banshees of Inisherin", 
+                    ImageName = "Images/BansheesInisherin.jpg", 
+                    Cast = "Colin Farrell, Brendan Gleeson, Kerry Condon, Barry Keoghan", 
+                    Description = "The Banshees of Inisherin is a 2022 black tragicomedy film directed by Martin McDonagh. Set on a remote, fictional island off the west coast of Ireland in 1923, the film stars Colin Farrell and Brendan Gleeson as two lifelong friends who find themselves at an impasse when one abruptly ends their relationship, with severe consequences for both of them. The movie explores the anguish and gravity of their breakup in a world where men don't usually express their feelings."
+                };
+                Movie m2 = new Movie()
+                {
+                    Title = "Aftersun",
+                    ImageName = "Images/Aftersun.jpg",
+                    Cast = "Paul Mescal, Frankie Corio, Celia Rowlson-Hall, Sally Messham",
+                    Description = "Aftersun is a 2022 semi-autobiographical coming-of-age drama film written and directed by Charlotte Wells in her feature directorial debut. Starring Paul Mescal, Frankie Corio, and Celia Rowlson-Hall, the film is loosely based on Wells' childhood and follows an 11-year-old Scottish girl on holiday with her father at a Turkish resort on the eve of his 31st birthday."
+                };
+                Movie m3 = new Movie()
+                {
+                    Title = "The Quiet Girl",
+                    ImageName = "Images/QuietGuirl.jpg",
+                    Cast = "Carrie Crowley, Andrew Bennett, Catherine Clinch",
+                    Description = "The Quiet Girl is a 2022 Irish coming-of-age drama film written and directed by Colm Bairéad, in his feature film debut.[1][2] Based on the 2010 short story \"Foster\" by Claire Keegan, it stars Catherine Clinch, Carrie Crowley and Andrew Bennett."
+                };
+
+                db.Movies.Add(m1);
+                db.Movies.Add(m2);
+                db.Movies.Add(m3);
+
+                Booking b1 = new Booking() { BookingDate = new DateTime(2025,07,10), Movie = m1, NumberOfTickets = 2 };
+                Booking b2 = new Booking() { BookingDate = new DateTime(2025, 07, 10), Movie = m1, NumberOfTickets = 6 };
+                Booking b3 = new Booking() { BookingDate = new DateTime(2025, 04, 09), Movie = m2, NumberOfTickets = 3 };
+                Booking b4 = new Booking() { BookingDate = new DateTime(2025, 04, 10), Movie = m3, NumberOfTickets = 2 };
+
+                db.Bookings.Add(b1);
+                db.Bookings.Add(b2);
+                db.Bookings.Add(b3);
+                db.Bookings.Add(b4);
+
+                db.SaveChanges();
+                Console.WriteLine("Data base created");
+            }
+        }
+    }
+}
---
+++ Week12/DataManagement/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DataManagement")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ATUS")]
+[assembly: AssemblyProduct("DataManagement")]
+[assembly: AssemblyCopyright("Copyright © ATUS 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a2716611-e5fd-4aff-bdf0-4aa1b822f1ec")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
